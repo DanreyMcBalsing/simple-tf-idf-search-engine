0a36e61b1a9fb89e821d2aeac49b4d6c7948986b
Ali Ghasemi <33260755+DanreyMcBalsing@users.noreply.github.com>
2019-09-18 14:07:37 +0430

    Update tf-idf.py

--- tf-idf.py
+++ tf-idf.py
@@ -7,9 +7,9 @@
 import math
 import re
 import operator
-with open("DS/Corpus.txt","r+") as a:#importing the corpus
+with open("Corpus.txt","r+") as a:#importing the corpus
     corpus=a.read()
-with open("DS/Stopwords.txt","r+") as b: #importing stopwords
+with open("Stopwords.txt","r+") as b: #importing stopwords
     stopwords=b.read()
 
 
